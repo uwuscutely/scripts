eb3493b05a1c926a8f19713e28e80b06a677c5dc
uwuscutely <shenlain.nyc@gmail.com>
2021-04-07 02:57:01 -0400

    Update DAEaster.lua

--- DAEaster.lua
+++ DAEaster.lua
@@ -2,7 +2,7 @@ local library = loadstring(game:HttpGet(("https://raw.githubusercontent.com/Aika
 
 local w = library:CreateWindow("Dragon Adventures") -- Creates the window
 
-local b = w:CreateFolder("Egg TP") -- Creates the folder(U will put here your buttons,etc)
+local b = w:CreateFolder("UwUs Cutely") -- Creates the folder(U will put here your buttons,etc)
 
 b:Button(
 "Easter Egg TP",
@@ -12,7 +12,7 @@ function()
    local TwT = ReplicatedStorage.EasterEventStorage:WaitForChild("GetEggRemote")
 
    for i, v in pairs(Workspace:GetDescendants()) do
-      if v:IsA("MeshPart") and v.MeshId == "rbxassetid://6563267204" and v.Name ~= "Egg" then
+      if v:IsA("MeshPart") and v.MeshId == "rbxassetid://6540319837" and v.Name ~= "Egg" then
          noob.Character.HumanoidRootPart.CFrame = v.CFrame
          wait()
          TwT:FireServer(v.Parent)
@@ -22,14 +22,14 @@ end
 )
 
 b:Button(
-"Danger Egg TP",
+"Chaos Egg TP",
 function()
    local noob = game.Players.LocalPlayer
    local ReplicatedStorage = game:GetService("ReplicatedStorage")
    local OwO = ReplicatedStorage.EasterEventStorage:WaitForChild("GetCalendarEggRemote")
 
    for i, v in pairs(Workspace:GetDescendants()) do
-      if v:IsA("MeshPart") and v.MeshId == "rbxassetid://6540320264" and v.Name ~= "Egg" then
+      if v:IsA("MeshPart") and v.MeshId == "rbxassetid://6540320456" and v.Name ~= "Egg" then
          noob.Character.HumanoidRootPart.CFrame = v.CFrame
          wait()
          local UwU = OwO:InvokeServer(v.Parent)
@@ -54,5 +54,22 @@ function()
    end
 end
 )
+
+b:Toggle("Toggle",function(bool)
+   shared.toggle = bool
+   while true do
+      for _,v in pairs(game:GetService("Workspace").HarvestZones.Fruit.Drops.Small:GetDescendants()) do
+         if v:IsA("MeshPart")    then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+            wait(0.5)
+            fireclickdetector(game:GetService("Workspace").HarvestZones.Fruit.Drops.Small.ClickDetector)
+            wait(8)
+			if shared.toggle == false then
+			   break
+			end
+         end
+      end
+   end
+end)
 
 b:DestroyGui()
