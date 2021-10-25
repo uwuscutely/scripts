606bd5df78e94b1ddd9f47fa5b82720054f15501
victorr <shenlain.nyc@gmail.com>
2021-10-25 15:31:58 -0400

    Create WZHalloween.lua

---
+++ WZHalloween.lua
@@ -0,0 +1,18 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local Player = game:GetService("Players").LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local character = Player.Character or Player.CharacterAdded:Wait()
+local MissionFinished = ReplicatedStorage.Shared.Missions:WaitForChild("MissionFinished")
+local LeaveChoice = ReplicatedStorage.Shared.Missions:WaitForChild("LeaveChoice")
+local NotifyReadyToLeave = ReplicatedStorage.Shared.Missions:WaitForChild("NotifyReadyToLeave")
+local profile = ReplicatedStorage.Profiles:WaitForChild(Player.Name)
+
+if profile.Level.Value <= 99 then
+    MissionFinished.OnClientEvent:Connect(function()
+        LeaveChoice:FireServer("true")
+        NotifyReadyToLeave:FireServer()
+    end)
+end
