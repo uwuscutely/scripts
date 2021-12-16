88777e0367fc8d639f8010a80248f6a7c1ba8645
victorr <shenlain.nyc@gmail.com>
2021-12-16 04:34:30 -0500

    Update WZGuildAuto.lua

--- WZGuildAuto.lua
+++ WZGuildAuto.lua
@@ -12,149 +12,155 @@ local ActiveMission = ReplicatedStorage:WaitForChild("ActiveMission")
 local PlaceId = game.PlaceId
 
 local function oneDifficulty()
-	local difficulty = GetDifficulty:InvokeServer()
-	local missionID = ActiveMission.Value
-	print("UwU", "OwO", missionID, difficulty)
-	
-	if difficulty < 4 then
-		MissionFinished.OnClientEvent:Connect(function()
-			StartRaid:FireServer(missionID + 1, 1)
-		end)
-	end
+    local difficulty = GetDifficulty:InvokeServer()
+    local missionID = ActiveMission.Value
+    print("UwU", "OwO", missionID, difficulty)
+
+    if difficulty < 4 then
+        MissionFinished.OnClientEvent:Connect(function()
+            StartRaid:FireServer(missionID + 1, 1)
+        end)
+    end
 end
 
 local function twoDifficulties()
-	local difficulty = GetDifficulty:InvokeServer()
-	local missionID = ActiveMission.Value
-	print("UwU", "OwO", missionID, difficulty)
-	
-	if difficulty < 2 then
-		MissionFinished.OnClientEvent:Connect(function()
-			StartRaid:FireServer(missionID, difficulty + 1)
-		end)
-	else
-		MissionFinished.OnClientEvent:Connect(function()
-			StartRaid:FireServer(missionID + 1, 1)
-		end)
-	end
+    local difficulty = GetDifficulty:InvokeServer()
+    local missionID = ActiveMission.Value
+    print("UwU", "OwO", missionID, difficulty)
+
+    if difficulty < 2 then
+        MissionFinished.OnClientEvent:Connect(function()
+            StartRaid:FireServer(missionID, difficulty + 1)
+        end)
+    else
+        MissionFinished.OnClientEvent:Connect(function()
+            StartRaid:FireServer(missionID + 1, 1)
+        end)
+    end
 end
 
 local function threeDifficulties()
-	local difficulty = GetDifficulty:InvokeServer()
-	local missionID = ActiveMission.Value
-	print("UwU", "OwO", missionID, difficulty)
-	
-	if difficulty < 3 then
-		MissionFinished.OnClientEvent:Connect(function()
-			StartRaid:FireServer(missionID, difficulty + 1)
-		end)
-	else
-		MissionFinished.OnClientEvent:Connect(function()
-			StartRaid:FireServer(missionID + 1, 1)
-		end)
-	end
+    local difficulty = GetDifficulty:InvokeServer()
+    local missionID = ActiveMission.Value
+    print("UwU", "OwO", missionID, difficulty)
+
+    if difficulty < 3 then
+        MissionFinished.OnClientEvent:Connect(function()
+            StartRaid:FireServer(missionID, difficulty + 1)
+        end)
+    else
+        MissionFinished.OnClientEvent:Connect(function()
+            StartRaid:FireServer(missionID + 1, 1)
+        end)
+    end
 end
 
 local function fourDifficulties()
-	local difficulty = GetDifficulty:InvokeServer()
-	local missionID = ActiveMission.Value
-	print("UwU", "OwO", missionID, difficulty)
-	
-	if difficulty < 4 then
-		MissionFinished.OnClientEvent:Connect(function()
-			StartRaid:FireServer(missionID, difficulty + 1)
-		end)
-	else
-		MissionFinished.OnClientEvent:Connect(function()
-			if missionID == 4 then
-				StartRaid:FireServer(6, 1)
-			elseif missionID == 16 then
-				StartRaid:FireServer(18, 1)
-			elseif missionID == 20 then
-				StartRaid:FireServer(21)
-			else
-				StartRaid:FireServer(missionID + 1, 1)
-			end
-		end)
-	end
+    local difficulty = GetDifficulty:InvokeServer()
+    local missionID = ActiveMission.Value
+    print("UwU", "OwO", missionID, difficulty)
+
+    if difficulty < 4 then
+        MissionFinished.OnClientEvent:Connect(function()
+            StartRaid:FireServer(missionID, difficulty + 1)
+        end)
+    else
+        MissionFinished.OnClientEvent:Connect(function()
+            if missionID == 4 then
+                StartRaid:FireServer(6, 1)
+            elseif missionID == 20 then
+                StartRaid:FireServer(21)
+            else
+                StartRaid:FireServer(missionID + 1, 1)
+            end
+        end)
+    end
 end
 
 local function fiveDifficulties()
-	local difficulty = GetDifficulty:InvokeServer()
-	local missionID = ActiveMission.Value
-	print("UwU", "OwO", missionID, difficulty)
-	
-	if difficulty < 5 then
-		if difficulty == 2 and PlaceId == 6847034886 then
-			MissionFinished.OnClientEvent:Connect(function()
-				StartRaid:FireServer(missionID, 5)
-			end)
-		else
-			MissionFinished.OnClientEvent:Connect(function()
-				StartRaid:FireServer(missionID, difficulty + 1)
-			end)
-		end
-	else
-		MissionFinished.OnClientEvent:Connect(function()
-			if missionID == 7 then
-				StartRaid:FireServer(11, 1)
-			elseif missionID == 26 then
-				StartRaid:FireServer(27)
-			else
-				StartRaid:FireServer(missionID + 1, 1)
-			end
-		end)
-	end
+    local difficulty = GetDifficulty:InvokeServer()
+    local missionID = ActiveMission.Value
+    print("UwU", "OwO", missionID, difficulty)
+
+    if difficulty < 5 then
+        if difficulty == 2 and (PlaceId == 6847034886 or 6386112652 or 6510862058) then
+            MissionFinished.OnClientEvent:Connect(function()
+                StartRaid:FireServer(missionID, 5)
+            end)
+        else
+            MissionFinished.OnClientEvent:Connect(function()
+                StartRaid:FireServer(missionID, difficulty + 1)
+            end)
+        end
+    else
+        MissionFinished.OnClientEvent:Connect(function()
+            if missionID == 7 then
+                StartRaid:FireServer(11, 1)
+            elseif missionID == 16 then
+                StartRaid:FireServer(18, 1)
+            elseif missionID == 26 then
+                StartRaid:FireServer(27)
+            else
+                StartRaid:FireServer(missionID + 1, 1)
+            end
+        end)
+    end
 end
 
 local function tower()
-	local missionID = ActiveMission.Value
-	print("UwU", "OwO", missionID)
-	MissionFinished.OnClientEvent:Connect(function()
-		if missionID == 21 then
-			StartRaid:FireServer(26, 1)
-		end
-	end)
+    local missionID = ActiveMission.Value
+    local TowerFinished = ReplicatedStorage.MissionScripts[missionID]:WaitForChild("TowerFinished")
+    print("UwU", "OwO", missionID)
+    TowerFinished.OnClientEvent:Connect(function()
+        if missionID == 21 then
+            wait(15)
+            StartRaid:FireServer(24, 1)
+        end
+    end)
 end
-	
 
 if PlaceId == 2978696440 then -- Crabby
-	oneDifficulty()
+    oneDifficulty()
 elseif PlaceId == 4310476380 then -- Scarecrow
-	oneDifficulty()
+    oneDifficulty()
 elseif PlaceId == 4310464656 then -- Dire Prob
-	fourDifficulties()
+    fourDifficulties()
 elseif PlaceId == 4310478830 then -- Kingslayer
     fourDifficulties()
 elseif PlaceId == 3383444582 then -- Gravetower
-	fiveDifficulties()
+    fiveDifficulties()
 elseif PlaceId == 3165900886 then -- Volcano Dungeon
-	fiveDifficulties()
+    fiveDifficulties()
 elseif PlaceId == 3885726701 then -- Ruin
-	fourDifficulties()
+    fourDifficulties()
 elseif PlaceId == 3994953548 then -- Mama
-	fourDifficulties()
+    fourDifficulties()
 elseif PlaceId == 4050468028 then -- Volcano's Shadow
-	fiveDifficulties()
+    fiveDifficulties()
 elseif PlaceId == 4465988196 then -- Mountain Pass
-	fourDifficulties()
+    fourDifficulties()
 elseif PlaceId == 4465989351 then -- Winter Cavern
-	fourDifficulties()
+    fourDifficulties()
 elseif PlaceId == 4465989998 then -- Winter Dungeon
-	fourDifficulties()
+    fiveDifficulties()
 elseif PlaceId == 4646475570 then -- Pyramid Dungeon
-	fiveDifficulties()
+    fiveDifficulties()
 elseif PlaceId == 4646475342 then -- Deserted
-	fourDifficulties()
+    fourDifficulties()
 elseif PlaceId == 4646473427 then -- Scrap
-	fourDifficulties()
+    fourDifficulties()
 elseif PlaceId == 5703353651 then -- Prison Tower
-	tower()
+    tower()
+elseif PlaceId == 6386112652 then -- Konoh
+    fiveDifficulties()
+elseif PlaceId == 6510862058 then -- Rough Waters
+    fiveDifficulties()
 elseif PlaceId == 6847034886 then -- Underworld
-	fiveDifficulties()
+    fiveDifficulties()
 elseif PlaceId == 7071564842 then -- Mez Tower
-	tower()
-	MissionFinished.OnClientEvent:Connect(function()
-		print("Wow the script worked.. Meow meow..")
+    local TowerFinished = ReplicatedStorage.MissionScripts[missionID]:WaitForChild("TowerFinished")
+    tower()
+    TowerFinished.OnClientEvent:Connect(function()
+        print("Wow the script worked.. Meow meow..")
     end)
 end
